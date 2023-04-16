ea0f190c85c6f86fa14cbca4b5d55499367aeff4
T0Tlll <36969795@qq.com>
2023-04-16 22:31:21 +0800

    单击弹出隐藏

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,19 +1,33 @@
-import ListGroup from "./components/ListGroup";
+// import Alert from "./components/Alert";
+import { useState } from "react";
+import Button from "./components/Button";
+import Alert from "./components/Alert";
+// import ListGroup from "./components/ListGroup";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
-
+  // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+  const [alertVisible, setAlerVisibility] = useState(false);
   return (
     <div>
-      <ListGroup
+      {alertVisible && (
+        <Alert onClose={() => setAlerVisibility(false)}>My alert</Alert>
+      )}
+      <Button color="primary" onClick={() => setAlerVisibility(true)}>
+        children
+      </Button>
+
+      {/* <Alert>
+        Hello <span>Word</span>
+      </Alert> */}
+
+      {/* <ListGroup
         items={items}
         heading="Cities"
         onSelectItem={handleSelectItem}
-      />
+      /> */}
     </div>
   );
 }
